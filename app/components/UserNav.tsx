0f75fcfefe99bbeba9b3a151925342f533cd5f7e
abhishek <abhishekpl8219@gmail.com>
2024-05-21 08:19:03 +0530

    pushed the code for production

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -14,26 +14,27 @@ import {
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
 import { createAirbnbHome } from "../action";
+import Image from "next/image";
 
 export async function UserNav() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-  const createHomeWithId = createAirbnbHome.bind(null,{
-    userId:user?.id as string,
-  })
+  const createHomeWithId = createAirbnbHome.bind(null, {
+    userId: user?.id as string,
+  });
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex gap-x-3 items-center">
           <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5 " />
-          <img
+          <Image
             className="rounded-full h-8 w-8 hidden lg:block"
             src={
               user?.picture ??
               "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
             }
             alt="image of the user "
-          ></img>
+          ></Image>
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[200px]">
